63247388f308014ad584cdb8af8229c32db11aec
Ryougi-G <1365654168@qq.com>
2020-07-17 18:40:03 +0800

    创建项目

---
+++ SophieHTTP/HTTPResolve/HTTPHeader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SophieHTTP.HTTPResolve
+{
+    abstract class HTTPHeader
+    {
+        protected string HeaderKey;
+        protected Object HeaderValue;
+        public virtual string Key
+        {
+            get
+            {
+                return Key;
+            }
+            set
+            {
+                HeaderKey = value;
+            }
+        }
+        public virtual Object Value
+        {
+            get
+            {
+                return HeaderValue;
+            }
+            set
+            {
+                HeaderValue = value;
+            }
+        }
+        public abstract string getHeaderString();
+    }
+}
---
+++ SophieHTTP/HTTPResolve/TestHeader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SophieHTTP.HTTPResolve
+{
+    class TestHeader : HTTPHeader
+    {
+        public override string getHeaderString()
+        {
+            return this.Key + ":" + this.Value.ToString();
+        }
+    }
+}
