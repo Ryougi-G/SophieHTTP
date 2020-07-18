9b33ccf19c33e6c53047ef79de2bd21674ace582
Ryougi-G <1365654168@qq.com>
2020-07-18 13:58:41 +0800

    添加HTTP请求方法

---
+++ SophieHTTP/HTTPResolve/CommonHeader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SophieHTTP.HTTPResolve
+{
+    class CommonHeader : HTTPHeader
+    {
+        public override object Value {
+            get
+            {
+                return HeaderValue;
+            }
+            set
+            {
+                HeaderValue = value;
+            }
+        }
+        public override string getHeaderString()
+        {
+            return Key + ":" + (string)HeaderValue;
+        }
+    }
+}
---
+++ SophieHTTP/HTTPResolve/HTTPRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SophieHTTP.HTTPResolve
+{
+    class HTTPRequest
+    {
+        
+        List<HTTPHeader> Headers;
+    }
+}
